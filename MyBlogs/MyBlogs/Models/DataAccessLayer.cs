d4df3a602f1120ac50ec25e2c803ac56f409b204
brijeshsubhagan <38424630+brijeshsubhagan@users.noreply.github.com>
2018-05-22 14:49:00 +0530

    Add files via upload

---
+++ MyBlogs/MyBlogs/Models/DataAccessLayer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace MyBlogs.Models
+{
+    public class DataAccessLayer
+    {
+        string connectionString = "Server= localhost; Database= Blog; Integrated Security=True;";
+        //public int AddEmployee(BlogUser blogUser)
+        //{
+        //    try
+        //    {
+        //        using (SqlConnection con = new SqlConnection(connectionString))
+        //        {
+        //            SqlCommand cmd = new SqlCommand("spAddBlogUser", con);
+        //            cmd.CommandType = CommandType.StoredProcedure;
+        //            cmd.Parameters.AddWithValue("@BlogUserName", blogUser.name);
+        //            cmd.Parameters.AddWithValue("@BlogUserPassword", blogUser.password);
+        //            cmd.Parameters.AddWithValue("@BlogUserEmailID", blogUser.email);
+        //            con.Open();
+        //            cmd.ExecuteNonQuery();
+        //            con.Close();
+        //        }
+        //        return 1;
+        //    }
+        //    catch
+        //    {
+        //        throw;
+        //    }
+        //}
+    }
+}
